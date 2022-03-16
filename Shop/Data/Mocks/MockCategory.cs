895c8be7fae6ea2b7e2ce1291978a175e0cd88a2
GORYNTCHdotNET <92940275+GORYNTCHdotNET@users.noreply.github.com>
2022-03-17 00:01:56 +0300

    first commit

---
+++ Shop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> ALLCategories()
+        {
+            return new List<Category>
+            {
+
+            }
+        }
+    }
+}
